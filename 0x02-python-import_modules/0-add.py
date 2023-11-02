3f8d50c5ef1de64e7a79263ff5ccfbb740bf7d34
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-11-02 10:38:06 -0700

    add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-/* a program that imports the function def add(a, b): from
-the file add_0.py and prints the result of the addition 1 + 2 = 3
-*/
-a = 1, b = 2
+if __name__ == "__main__":
+    from add_0 import add
 
-from add_0 import add
+    a = 1
+    b = 2
 
-result = add(a, b)
-print("{} + {} = {}")
+    print("{:c} + {:c} = {:c}".format(a, b, add(a, b)))
