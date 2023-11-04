54bc7db82a40457febb8c2624ddf9cd9fd334c6a
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-11-04 03:52:00 -0700

    3-infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    m = len(argv) - 1
+    if m == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        x = 1
+        add = 0
+        while x <= m:
+            add += int(argv[x])
+            x += 1
+        print("{:d}".format(add))
+
+
 if __name__ == "__main__":
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    add_arg(sys.argv)
